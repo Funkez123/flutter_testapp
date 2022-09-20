f39ada2e05c55a515dc761142b3188f0edbfa991
Daniel Anders <daniel.anders@droidsolutions.de>
2022-09-20 14:32:23 +0200

    step 3 completed

--- lib/favpage.dart
+++ lib/favpage.dart
@@ -18,15 +18,21 @@ late List future_fav_array;
 
 Future<List> getJson() async {
 
+    Hive.initFlutter();
+    var favbox = await Hive.openBox('favBox');
+    boxlength = favbox.get("favorites").length;
+    List fav_array = favbox.get("favorites");
+    future_fav_array = fav_array;
 
     var data = await rootBundle.loadString('DemoData.json');
     var jsondata = await jsonDecode(data);
 
     List<Article> articles = [];
 
-    for (var i in jsondata) {
-      Article article = Article(i["id"], i["title"], i["description_short"],
-          i["description"], i["text"], i["imageURL"]);
+
+    for(var x=0; x<boxlength; x++){
+
+      Article article = Article(jsondata[fav_array[x]-1]["id"], jsondata[fav_array[x]-1]["title"], jsondata[fav_array[x]-1]["description_short"],jsondata[fav_array[x]-1]["description"], jsondata[fav_array[x]-1]["text"], jsondata[fav_array[x]-1]["imageURL"]);
 
       articles.add(article);
     }
@@ -52,89 +58,89 @@ class _favpageState extends State<favpage> {
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
       body: FutureBuilder(
-        future: getFavId(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          return ListView.builder(itemBuilder: (BuildContext context, int index) {
-          return Card(
-            elevation: 2,
-            child: FutureBuilder(
-              future: getFavId(),
+              future: getJson(),
               builder: (BuildContext context, AsyncSnapshot snapshot) {
-                
                 if (snapshot.data == null) {
-              return const Center(child: Text("Loading"));
-            } else {
-                
-                 
-                 return FutureBuilder(
-            future: getJson(),
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.data == null || future_fav_array == []) {
-              return const Center(child: Text("Loading"));
-            } else {
-              return Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                                children: [
-                                  Hero(
-                                      tag: "img${snapshot.data[index].id}",
-                                      child: Image.network(
-                                        snapshot.data[future_fav_array[index]-1].imgurl,
-                                        loadingBuilder: (BuildContext context,
-                                            Widget child,
-                                            ImageChunkEvent?
-                                                loadingProgress) {
-                                          if (loadingProgress == null)
-                                            return child;
-                                          return Center(
-                                            child: CircularProgressIndicator(
-                                              value: loadingProgress
-                                                          .expectedTotalBytes !=
-                                                      null
-                                                  ? loadingProgress
-                                                          .cumulativeBytesLoaded /
-                                                      loadingProgress
-                                                          .expectedTotalBytes!
-                                                  : null,
-                                            ),
-                                          );
-                                        },
-                                      )),
-                                  Align(
-                                      alignment: Alignment(-1, 0),
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text(
-                                          snapshot.data[index].title,
-                                          style: TextStyle(
-                                              fontSize: 22,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                      )),
-                                  Divider(
-                                    thickness: 1,
-                                  ),
-                                  Align(
-                                      alignment: Alignment(-1, 0),
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text(
-                                          snapshot.data[index].shortdesc,
-                                          style: TextStyle(fontSize: 18),
-                                        ),
-                                      )),
-                                ],
-                              ),
-              );
-            }
-            });}}),
-          );
-          },
-      
-          itemCount: boxlength,
-        ); },
-        
-      ),
+                  return Container(child: Center(child: Text("Loading")));
+                } else {
+                  return ListView.builder(
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return GestureDetector(
+                          onTap: () async {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => DetailPage(
+                                          id: snapshot.data[index].id,
+                                          title: snapshot.data[index].title,
+                                          description:
+                                              snapshot.data[index].longdesc,
+                                          imgurl: snapshot.data[index].imgurl,
+                                          hero_id:
+                                              "img${snapshot.data[index].id}",
+                                        )));
+                          },
+                          child: Card(
+                              elevation: 2,
+                              child: Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Column(
+                                  children: [
+                                Hero(
+                                    tag: "img${snapshot.data[index].id}",
+                                    child: Image.network(
+                                      snapshot.data[index].imgurl,
+                                      loadingBuilder: (BuildContext context,
+                                          Widget child,
+                                          ImageChunkEvent?
+                                              loadingProgress) {
+                                        if (loadingProgress == null)
+                                          return child;
+                                        return Center(
+                                          child: CircularProgressIndicator(
+                                            value: loadingProgress
+                                                        .expectedTotalBytes !=
+                                                    null
+                                                ? loadingProgress
+                                                        .cumulativeBytesLoaded /
+                                                    loadingProgress
+                                                        .expectedTotalBytes!
+                                                : null,
+                                          ),
+                                        );
+                                      },
+                                    )),
+                                Align(
+                                    alignment: Alignment(-1, 0),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        snapshot.data[index].title,
+                                        style: TextStyle(
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    )),
+                                Divider(
+                                  thickness: 1,
+                                ),
+                                Align(
+                                    alignment: Alignment(-1, 0),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        snapshot.data[index].shortdesc,
+                                        style: TextStyle(fontSize: 18),
+                                      ),
+                                    )),
+                                  ],
+                                ),
+                              )),
+                        );
+                      });
+                }
+              }),
     );
   }
 }
